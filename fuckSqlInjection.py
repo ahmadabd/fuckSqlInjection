048db8d1998c6f03d8ddcbacd099fbd90bf9f579
alux <ahmadabd13741112@gmail.com>
2019-09-05 12:20:25 +0430

    make simple class for start

---
+++ fuckSqlInjection.py
@@ -0,0 +1,20 @@
+## Remove sql command in lowerCase or upperCase from user input
+
+class fuckSqlInjection:
+    def __init__(self):
+        self.badChars = ["select","union", "drop", ";", "--", "insert", "delete", "xp_", "#", "%", "&", \
+            "'", "(", ")", "/", "\\", ":", ";", "<", ">", "=", "[", "]", "?", "`", "|", "<=", ">=", \
+            "*", "as", "in", "between"]
+
+    def checkVul(self, userInput):
+        inp = userInput.split(" ")
+
+        for char in self.badChars:
+            if char in inp:
+                inp.remove(char)
+            if char.upper() in inp:
+                inp.remove(char.upper())
+
+        inp = " ".join(inp)
+
+        return inp
