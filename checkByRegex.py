70b34f04812ba3b244bb03518389981b3e490311
alux <ahmadabd13741112@gmail.com>
2019-09-30 15:11:07 +0330

    add regex for email and password

--- checkByRegex.py
+++ checkByRegex.py
@@ -3,8 +3,22 @@
 
 import re
 
-def checkInput(code):
+def checkCode(code):
     if re.search("^[0-9]{4}$", code):
+        return True
+    else:
+        return False
+
+
+def checkPassword(password):
+    if re.search("^[a-zA-Z0-9.$#]{4, 16}$", password):
+        return True
+    else:
+        return False
+
+
+def checkEmail(email):
+    if re.search("^[a-zA-Z0-9]+\@[a-zA-Z0-9]+.[a-z]+$", email):
         return True
     else:
         return False
